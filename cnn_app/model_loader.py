cabac46d2822961e0b4e38a591434d395a63e606
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-04-26 18:15:23 -0500

    Update model file name in model_loader.py

--- cnn_app/model_loader.py
+++ cnn_app/model_loader.py
@@ -1,5 +1,7 @@
 from keras.models import load_model
 
 print("Cargando modelo")
-model = load_model("cnn_app/data/model_Mnist.h5")
+model = load_model("cnn_app/data/model_Mnist_LeNet.h5")
+# model = load_model("cnn_app/data/model_Mnist.h5")
+
 
